1546c7751029461d82ea59dd7ed30be2754d835b
Chris Lee <41558581+z5210274@users.noreply.github.com>
2025-11-27 16:26:16 +1100

    Delete src/wall_follower directory

--- src/wall_follower/startup.bash
+++
@@ -1,5 +0,0 @@
-ros2 launch turtlebot3_gazebo turtlebot3_maze.launch.py &
-sleep 20
-ros2 launch turtlebot3_cartographer cartographer.launch.py use_sim_time:=True &
-sleep 25
-ros2 launch wall_follower wall_follower.launch.py &
